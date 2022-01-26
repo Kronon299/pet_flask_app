7a71535051da8b3ef20a5800f8f6c05f31117b1d
Stanislau Klimovich <stasklimovich299@gmail.com>
2022-01-26 22:29:18 +0300

    simple rest app created

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from app import app
+from app import app, api, Quote
+
+api.add_resource(Quote, "/ai-quotes", "/ai-quotes/", "/ai-quotes/<int:id>")
 
 if __name__ == '__main__':
     app.run(
         host="0.0.0.0",
         debug=True
     )
-
-
-    # def foo():
-    #     pass
